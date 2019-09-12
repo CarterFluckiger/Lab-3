ee732fade3ef0b034e88d89465c0fe9554cb1b18
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2019-09-12 12:48:33 -0500

    Initial Commit

---
+++ Lab 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 3
+//
+//  Created by Tag1 on 9/12/19.
+//  Copyright © 2019 Checkers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
