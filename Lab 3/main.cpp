8e91b6222c83648c20d2e17a0191f598d22ca098
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2019-09-26 14:11:25 -0500

    New

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -12,63 +12,103 @@
 #include <fstream>
 using namespace std;
 
+
+// The floats for the inputs from the user and the mean of those values
+   int userin1;
+   int userin2;
+   int userin3;
+   int userin4;
+   float mean;
+// The floats for the values of the userinputs that get subtracted by the mean
+float pow1;
+float pow2;
+float pow3;
+float pow4;
+
+// The floats for the values that get squared
+float mean1;
+float mean2;
+float mean3;
+float mean4;
+float meanf;
+
+int input1f;
+int input2f;
+int input3f;
+int input4f;
+
+void average(int one, int two, int three,int four);
+void calculation(int one, int two, int three,int four);
+void fileinput();
+void user();
+
 int main()
 {
-    // The floats for the inputs from the user and the mean of those values
-    int userin1;
-    int userin2;
-    int userin3;
-    int userin4;
-    float mean;
+    user();
+    average(userin1, userin2, userin3, userin4);
+    calculation(userin1, userin2, userin3, userin4);
+    fileinput();
+    average(input1f,input2f, input3f, input4f);
+    fileinput();
+    calculation(input1f, input2f, input3f, input4f);
+    
+    string fileoutput ="/Users/tag1/Desktop/Lab 3/Lab 3/outMeanStd.dat";
+       ofstream outFile;
+       outFile.open(fileoutput);
+    outFile<< average(userin1, userin2, userin3, userin4);
+    
+    outFile.close();
+    
+    return 0;
+}
+void fileinput()
+{
     
     
+    string fileinput ="/Users/tag1/Desktop/Lab 3/Lab 3/index.dat";
+    ifstream inFile;
+    inFile.open(fileinput);
+    inFile>>input1f>> input2f>>input3f>>input4f;
     
-    // The floats for the values of the userinputs that get subtracted by the mean
-    float pow1;
-    float pow2;
-    float pow3;
-    float pow4;
-    
-    // The floats for the values that get squared
-    float mean1;
-    float mean2;
-    float mean3;
-    float mean4;
-    float meanf;
-    
-    // Statement for the user to know they have to put in four numbers of their choice
-    cout<<"Please enter 4 integers: ";
-    // The inputs from the user to find the mean and the standard deviation
-    cin>>userin1;
-    cin>>userin2;
-    cin>>userin3;
-    cin>>userin4;
-    
-    
-    // The output that tells the user their mean
-    cout<<"Your mean is: "<< float(userin1 + userin2 + userin3 + userin4) / 4 << endl<< endl; 
-    
+}
+
+void average(int one, int two, int three, int four)
+{
+       
+    cout<<"Your mean is: "<< float(one + two + three + four) / 4 << endl<< endl;
+}
+
+void calculation(int one, int two, int three, int four)
+{
     // Statement calculates the mean
-    mean = float(userin1 + userin2 + userin3 + userin4) / 4;
-    
+    mean = float(one + two + three + four) / 4;
+
     // Calculations for inputs minus the mean
-    pow1 = (userin1 - mean);
-    pow2 = (userin2 - mean);
-    pow3 = (userin3 - mean);
-    pow4 = (userin4 - mean);
-    
+    pow1 = (one - mean);
+    pow2 = (two - mean);
+    pow3 = (three - mean);
+    pow4 = (four - mean);
+
     // Squaring the pow statements from above
     mean1 = pow(pow1,2);
     mean2 = pow(pow2,2);
     mean3 = pow(pow3,2);
     mean4 = pow(pow4,2);
-    
+
     
     // Finding the final mean for standard deviation and outputing it
     meanf = (mean1 + mean2 + mean3 + mean4)/ 4;
     cout<<"This is the standard deviation: ";
     cout<<  sqrt(meanf)<<endl<<endl;
-   
-   
-    return 0;
+
+}
+void user()
+{
+// Statement for the user to know they have to put in four numbers of their choice
+cout<<"Please enter 4 integers: ";
+// The inputs from the user to find the mean and the standard deviation
+cin>>userin1;
+cin>>userin2;
+cin>>userin3;
+cin>>userin4;
 }
